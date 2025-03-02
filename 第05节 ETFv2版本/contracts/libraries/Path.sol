89bb9308f78c6e099ce1f3f3709c57c743bf1ffe
SiLugang <167219827+SiLugang@users.noreply.github.com>
2025-03-02 22:32:41 +0800

    Update Path.sol

--- 第05节 ETFv2版本/contracts/libraries/Path.sol
+++ 第05节 ETFv2版本/contracts/libraries/Path.sol
@@ -22,14 +22,14 @@ library Path {
     /// @notice Returns true iff the path contains two or more pools
     /// @param path The encoded swap path
     /// @return True if path contains two or more pools, otherwise false
-    function hasMultiplePools(bytes memory path) internal pure returns (bool) {
+    function hasMultiplePools(bytes memory path) internal pure returns (bool) {//判断是否有多个池子的路径
         return path.length >= MULTIPLE_POOLS_MIN_LENGTH;
     }
 
     /// @notice Returns the number of pools in the path
     /// @param path The encoded swap path
     /// @return The number of pools in the path
-    function numPools(bytes memory path) internal pure returns (uint256) {
+    function numPools(bytes memory path) internal pure returns (uint256) {//路径有多少个池子组成
         // Ignore the first token address. From then on every fee and token offset indicates a pool.
         return ((path.length - ADDR_SIZE) / NEXT_OFFSET);
     }
@@ -39,16 +39,16 @@ library Path {
     /// @return tokenA The first token of the given pool
     /// @return tokenB The second token of the given pool
     /// @return fee The fee level of the pool
-    function decodeFirstPool(bytes memory path)
+    function decodeFirstPool(bytes memory path)//解码路径的第一个池子
         internal
         pure
         returns (
-            address tokenA,
-            address tokenB,
-            uint24 fee
+            address tokenA,//返回A代币地址
+            address tokenB,//返回B代币地址
+            uint24 fee//两个代币swap的费率
         )
     {
-        tokenA = path.toAddress(0);
+        tokenA = path.toAddress(0);//解码后的
         fee = path.toUint24(ADDR_SIZE);
         tokenB = path.toAddress(NEXT_OFFSET);
     }
@@ -56,14 +56,14 @@ library Path {
     /// @notice Gets the segment corresponding to the first pool in the path
     /// @param path The bytes encoded swap path
     /// @return The segment containing all data necessary to target the first pool in the path
-    function getFirstPool(bytes memory path) internal pure returns (bytes memory) {
+    function getFirstPool(bytes memory path) internal pure returns (bytes memory) {//获得第一个池子的路径，byte类型的（整个的）
         return path.slice(0, POP_OFFSET);
     }
 
     /// @notice Skips a token + fee element from the buffer and returns the remainder
     /// @param path The swap path
     /// @return The remaining token + fee elements in the path
-    function skipToken(bytes memory path) internal pure returns (bytes memory) {
+    function skipToken(bytes memory path) internal pure returns (bytes memory) {//第一个token跳过去，作用：第一个池子pool处理完，跳过，进入下一个池子处理，bytes memory：剩下的路径
         return path.slice(NEXT_OFFSET, path.length - NEXT_OFFSET);
     }
 }
