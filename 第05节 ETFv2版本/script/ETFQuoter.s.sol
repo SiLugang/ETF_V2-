b403ed5c1e9a2dab6cfea43ab785cff44234e3d3
SiLugang <167219827+SiLugang@users.noreply.github.com>
2025-03-05 16:28:15 +0800

    Update ETFQuoter.s.sol

--- 第05节 ETFv2版本/script/ETFQuoter.s.sol
+++ 第05节 ETFv2版本/script/ETFQuoter.s.sol
@@ -10,7 +10,7 @@ contract ETFQuoterScript is Script {
     address usdc;
 
     function setUp() public {
-        uniswapV3Quoter = 0x419D1c2331faAFDbdf9144C64a3E07f19D217ebD;
+        uniswapV3Quoter = 0x419D1c2331faAFDbdf9144C64a3E07f19D217ebD;//uniswapV3Quoter的合约地址
         weth9 = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
         usdc = 0x22e18Fc2C061f2A500B193E5dBABA175be7cdD7f;
     }
@@ -19,7 +19,7 @@ contract ETFQuoterScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        ETFQuoter etfQuoter = new ETFQuoter(uniswapV3Quoter, weth9, usdc);
+        ETFQuoter etfQuoter = new ETFQuoter(uniswapV3Quoter, weth9, usdc);//new一个ETFQouter
         console.log("ETFQuoter:", address(etfQuoter));
 
         vm.stopBroadcast();
